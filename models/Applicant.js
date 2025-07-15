c04d2a65cf51d56c82bab85bcddbc59949a8eb4a
LaisRochaS <laismaria713@gmail.com>
2025-07-15 12:19:20 -0400

    update project2

--- models/Applicant.js
+++ models/Applicant.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const applicantSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  resumeLink: {String} ,
+  resumeLink: {String},
   appliedDate: { type: Date, default: Date.now },
+  jobId: { type: mongoose.Schema.Types.ObjectId, ref: 'Job', required: true }
 });
 
 module.exports = mongoose.model('Applicant', applicantSchema);
