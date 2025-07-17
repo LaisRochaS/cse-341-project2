ec791802b5a018d21a7e73884e75b5f80292d37d
LaisRochaS <laismaria713@gmail.com>
2025-07-17 18:53:20 -0400

    up

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -14,15 +15,21 @@ app.use(express.json());
 app.use('/jobs', jobRoutes);
 app.use('/applicants', applicantRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('MongoDB connection error:', err);
+
+console.log('Connecting to MongoDB:', process.env.MONGODB_URL);
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
