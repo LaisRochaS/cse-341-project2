b84e834f6924a5b47184f99fc7ea720cbd8fc4c4
LaisRochaS <laismaria713@gmail.com>
2025-07-18 13:26:16 -0400

    up

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'API for managing contacts'
     },
     host: 'localhost:3000',
-    schemes: ['http', 'https']
+    schemes: [ 'https']
 }
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/applicantRoutes.js', './routes/jobRoutes.js'];
