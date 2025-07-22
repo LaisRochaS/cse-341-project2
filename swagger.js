55a2a6bd3fb543313950152d7297e8e5e457ecd2
LaisRochaS <laismaria713@gmail.com>
2025-07-22 14:17:59 -0400

    up

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Library API',
     description: 'API for managing books and authors',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: 'cse-341-project2-9u89.onrender.com/api-docs/',
+  schemes: ['http', 'https'],
 };
 
 const outputFile = './swagger.json';
