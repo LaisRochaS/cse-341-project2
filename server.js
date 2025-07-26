ed404f4731327ca6b3655b07f37fcae3dae75848
LaisRochaS <laismaria713@gmail.com>
2025-07-26 14:08:01 -0400

    up

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ const app = express();
 app.use(express.json());
 
 // Session + Passport
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
 app.use(passport.initialize());
 app.use(passport.session());
 
