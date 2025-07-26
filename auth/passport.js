96b4d9fe512c2154aaa4f723cc440a1922e7c5d1
LaisRochaS <laismaria713@gmail.com>
2025-07-26 14:46:31 -0400

    up

--- auth/passport.js
+++ auth/passport.js
@@ -1,7 +1,22 @@
+const passport = require('passport'); 
+const GitHubStrategy = require('passport-github2').Strategy;
+const User = require('../models/User');
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: '/auth/github/callback'
+  callbackURL: process.env.GITHUB_CALLBACK_URL || '/auth/github/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ githubId: profile.id });
@@ -12,8 +27,7 @@ passport.use(new GitHubStrategy({
       });
     }
     return done(null, user);
-  } catch (error) {
-    console.error('GitHub OAuth error:', error);
-    return done(error, null);
+  } catch (err) {
+    return done(err, null);
   }
 }));
