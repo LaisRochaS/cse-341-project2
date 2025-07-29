8cb27eba4262dc00528bae5770b0c842e1c9d9bf
LaisRochaS <laismaria713@gmail.com>
2025-07-29 14:50:18 -0400

    up

--- data/db.js
+++ data/db.js
@@ -1,17 +1,16 @@
-   const mongoose = require('mongoose');
-   require('dotenv').config();
+const mongoose = require('mongoose');
+require('dotenv').config(); 
 
-   const initializeDb = (callback) => {
-       mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-           .then(() => {
-               console.log("Database connected successfully");
-               callback(null);
-           })
-           .catch(err => {
-               console.error("Database connection error:", err);
-               callback(err);
-           });
-   };
+const initializeDb = (callback) => {
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Database connected successfully");
+            callback(null);
+        })
+        .catch(err => {
+            console.error("Database connection error:", err);
+            callback(err);
+        });
+};
 
-   module.exports = { initializeDb };
-   
+module.exports = { initializeDb };
